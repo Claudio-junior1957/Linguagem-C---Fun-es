fbdb5c55f43d000790832f4a2fb95783e1672f03
Claudio Junior <134631237+Claudio-junior1957@users.noreply.github.com>
2024-04-17 23:11:56 -0300

    Add files via upload

---
+++ Linguagem C - Funções/questao6.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<locale.h>
+#include<math.h>
+#define Pi 3.14
+
+float areacircunferencia(float r);
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	float r, a;
+	printf(">>>Cálculo da área da circunferencia<<<");
+	printf("\n\nDigite o valor do raio: ");
+	scanf("%f", &r);
+	
+	printf("\nA área da circunferencia é: %.1f", areacircunferencia(r));
+	
+	return(0);
+}
+
+float areacircunferencia(float r){
+	return(Pi * pow(r,2));
+}
